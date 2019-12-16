fcc8a6a8bf473ac64f9619da4878ba3bf003bc91
Jamie Penny <jamiepenny7@gmail.com>
2019-12-16 13:06:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,20 +1,4 @@
 # Define the variable board below.
 puts board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
-board[1] = 0
 
-board[2] = 1
-
-board[3] = 2
-
-board[4] = 3
-
-board[5] = 4
-
-board[6] = 5
-
-board[7] = 6
-
-board[8] = 7
-
-board[9] = 8
