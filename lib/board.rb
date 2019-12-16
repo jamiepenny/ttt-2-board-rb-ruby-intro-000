790f3aca731b5501a44ad1e4f7a78e7a80da0427
Jamie Penny <jamiepenny7@gmail.com>
2019-12-16 13:01:21 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,29 @@
 # Define the variable board below.
 puts board = ["0", "1", "2", "3", "4", "5", "6", "7", "8",]
+
+# Position 1: Left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: Right
+row[2]
+
+# Position 4: Left
+row[3]
+
+# Position 5: Middle
+row[4]
+
+# Position 6: Right
+row[5]
+
+# Position 7: Left
+row[6]
+
+# Position 8: Middle
+row[7]
+
+# Position 9: Right
+row[8]
